c0ee7111ce3119a21c5b7b4cb18bd52aa3bdfa3f
kike-algoclick <aldo.barriere2026@hilasal.superate.org.sv>
2026-05-17 01:08:59 -0600

    responsive navbar for phone finished

--- app/components/layout/navbars/landingnav.tsx
+++ app/components/layout/navbars/landingnav.tsx
@@ -1,46 +1,57 @@
 "use client";
 import Image from "next/image";
-import { Menu, X } from "lucide-react";
+import { Menu, X, House, SwatchBook, Gem } from "lucide-react";
 import { useState } from "react";
+
 
 
 export default function LandingNav() {
   const [open, setOpen] = useState(false);
   return (
-    <nav className=" fixed top-0 left-0 bg-wh-main text-sm text-black w-full min-height z-50 fixed  backdrop-blur-md">
-      <div className="flex items-center">
+    <nav className=" fixed top-0 left-0 bg-wh-main text-sm text-black w-full min-height z-50 fixed backdrop-blur-md">
+      <div className="flex items-center h-20">
         <div className="mt-3 flex-1 items-center justify-between">
-          <Image src={""} alt="Logo Image" width={"150"} />
+          <Image
+            src={"/valtrust-isologo.png"}
+            alt="Logo Image"
+            width={125}
+            height={125}
+          />
         </div>
 
         <div className="flex flex-col">
           <button
-            className="text-3xl md:hidden  items-center p-5 flex items-center flex-3z-50"
+            className="text-3xl md:hidden  items-center p-5 flex items-center flex-3z-50 z-20"
             onClick={() => setOpen(!open)}
           >
-            {open ? <X size={50} /> : <Menu size={50} />}
+            {open ? <X size={35} /> : <Menu size={35} className="text-bold " />}
           </button>
         </div>
       </div>
 
       {open && (
-        <div className=" flex text-black flex-col md:hidden delay-170 ease-in ease-out bg-wh-main items-center right-0 h-screen w-1/2 fixed gap-20 text-xl z-10">
-          <div className="flex flex-col items-center h-1/2 w-full divide-y-2 divide-bl-main  ">
-            <h3 className="p-10 w-full flex flex-col items-center">Home</h3>
-            <h3 className="p-10 w-full flex flex-col items-center">About Us</h3>
-            <h3 className="p-10 w-full flex flex-col items-center">Premium</h3>
-          </div>
-
-          <div className="flex flex-col gap-4 w-1/2">
-            <button className="bg-bl-main text-wh-main text-sm p-5 rounded-xl flex justify-center">
-              Sign In
-            </button>
-            <button className="bg-gray-400 text-wh-main p-5 text-sm rounded-xl flex justify-center">
-              Sign Up
-            </button>
+        <div className=" flex text-black flex-col md:hidden w-50 p-8 fixed bg-wh-main items-center top-5 right-4 gap-20 text-xl z-10 rounded-lg shadow-lg border-black border-solid border-2">
+          <div className="flex flex-col items-center text-left ">
+            <div className="w-full flex gap-2 justify-center items-center ">
+              <House size={30} />
+              <h3 className="w-full flex flex-col">Home</h3>
+            </div>
+            <div className="w-full flex gap-2 justify-center items-center">
+              <SwatchBook size={30} />
+              <h3 className="w-full flex flex-col">About Us</h3>
+            </div>
+            <div className="w-full flex gap-2 justify-center items-center">
+              <Gem size={30} />
+              <h3 className="w-full flex flex-col">Premium</h3>
+            </div>
           </div>
         </div>
       )}
+
+      <div className="flex md:hidden w-full items-center justify-center p-3 gap-5 text-lg">
+        <button className="p-5 bg-zinc-200 w-1/3 rounded-lg font-bold">Sign In</button>
+        <button className="p-5 bg-bl-main  w-1/3 rounded-lg text-white font-bold">Sign Up</button>
+      </div>
     </nav>
   );
 }
