8cfd5e66cb21fdbf8a56d5ebd988f8c01a9b0f57
kike-algoclick <aldo.barriere2026@hilasal.superate.org.sv>
2026-05-17 14:28:01 -0600

    landing page navbar finished

--- app/components/layout/navbars/landingnav.tsx
+++ app/components/layout/navbars/landingnav.tsx
@@ -10,7 +10,7 @@ export default function LandingNav() {
   return (
     <nav className=" fixed top-0 left-0 bg-wh-main text-sm text-black w-full min-height z-50 fixed backdrop-blur-md">
       <div className="flex items-center h-20">
-        <div className="mt-3 flex-1 items-center justify-between">
+        <div className="mt-3 w-1/3s items-center justify-between">
           <Image
             src={"/valtrust-isologo.png"}
             alt="Logo Image"
@@ -19,13 +19,34 @@ export default function LandingNav() {
           />
         </div>
 
-        <div className="flex flex-col">
+        <div className="md:block hidden w-2/3 text-md ">
+          <ul className="flex justify-end items-center gap-40">
+            <li className="hover:bg-zinc-300 p-3 rounded-4xl w-1/9 flex justify-center font-bold">
+              Home
+            </li>
+            <li className="hover:bg-zinc-300 p-3 rounded-4xl w-1/9 flex justify-center font-bold">
+              About Us
+            </li>
+            <li className="hover:bg-zinc-300 p-3 rounded-4xl w-1/9 flex justify-center font-bold">
+              Premium
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col w-full justify-end md:hidden ">
           <button
-            className="text-3xl md:hidden  items-center p-5 flex items-center flex-3z-50 z-20"
+            className="text-3xl md:hidden w-full justify-end items-center p-5 flex items-center flex-3z-50 z-20"
             onClick={() => setOpen(!open)}
           >
-            {open ? <X size={35} /> : <Menu size={35} className="text-bold " />}
+            {open ? <X size={35} /> : <Menu size={35} className="font-bold " />}
           </button>
+        </div>
+
+        <div className="flex justify-center md:flex hidden font-bold items-center w-1/3 gap-5 text-md">
+          <button className="w-1/3 bg-bl-main p-3 rounded-lg text-white">
+            Sign Up
+          </button>
+          <button className="w-1/3 bg-zinc-200 p-3 rounded-lg">Sign In</button>
         </div>
       </div>
 
@@ -49,8 +70,12 @@ export default function LandingNav() {
       )}
 
       <div className="flex md:hidden w-full items-center justify-center p-3 gap-5 text-lg">
-        <button className="p-5 bg-zinc-200 w-1/3 rounded-lg font-bold">Sign In</button>
-        <button className="p-5 bg-bl-main  w-1/3 rounded-lg text-white font-bold">Sign Up</button>
+        <button className="p-5 bg-zinc-200 w-1/3 rounded-lg font-bold">
+          Sign In
+        </button>
+        <button className="p-5 bg-bl-main  w-1/3 rounded-lg text-white font-bold">
+          Sign Up
+        </button>
       </div>
     </nav>
   );
