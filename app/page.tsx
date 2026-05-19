406a0095ee32d8c555a87229a5a03e681df4c2af
kike-algoclick <aldo.barriere2026@hilasal.superate.org.sv>
2026-05-19 01:30:26 -0600

    Landing finished with temporary images

--- app/page.tsx
+++ app/page.tsx
@@ -55,8 +55,74 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="bg-white h-screen text-black">
-<div className="text-lg flex items-center w-full mt-10 justify-start p-10 font-bold font-sans">Explore Our Communities</div>
+      <section className="bg-white p-5 text-black flex flex-col min-h-full p-20">
+        <div className="text-lg flex items-center w-full mt-10 justify-start p-10 font-bold font-sans">
+          Explore Our Communities
+        </div>
+
+        <div className="flex flex-col  w-full items-center justify-center md:flex-row gap-5 ">
+          <div className=" md:w-2/5 w-2/3 flex jusify-center flex-col shadow-2xl rounded-lg gap-2 transition-all duration-300 hover:scale-105">
+            <div className="relative w-full  h-100 ">
+              <Image
+                src={"/landing-image.png"}
+                alt={"Santa Tecla"}
+                fill
+                className="object-cover rounded-lg"
+              />
+            </div>
+            <h3 className="p-3 font-bold">Santa Tecla</h3>
+            <p className="p-2">Urban Area</p>
+          </div>
+
+          <div className="md:w-2/5 w-2/3 flex jusify-center flex-col shadow-2xl rounded-lg gap-2 transition-all duration-300 hover:scale-105">
+            <div className="relative w-full h-100 ">
+              <Image
+                src={"/landing-image.png"}
+                alt={"Santa Tecla"}
+                fill
+                className="object-cover rounded-lg"
+              />
+            </div>
+            <h3 className="p-3 font-bold">Santa Ana</h3>
+            <p className="p-2">Urban Area</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="flex flex-col md:flex-row items-center justify-center w-full bg-white text-black gap-5">
+        <div className="w-2/3  flex justify-center items-center flex-col mt-5 gap-5">
+          <h2 className="text-5xl font-bold flex justify-start w-full md:w-2/3 ">About Valtrust</h2>
+          <p className="text-2xl">
+            We are a digital platform that <br />
+            modernizes property buying, selling, <br />
+            and valuation in El Salvador through <br />
+            AI and real market data. We connect <br />
+            owners, buyers and investors in a <br />
+            secure and transparent environment,
+            <br />
+            making real state decisions easier <br />
+            and smarter.
+          </p>
+        </div>
+
+        <div className="w-full flex gap-10 items-center justify-center">
+          <div className="relative w-1/3 h-50 ">
+            <Image
+              src={"/landing-image.png"}
+              alt={"Santa Tecla"}
+              fill
+              className="object-cover rounded-lg"
+            />
+          </div>
+          <div className="relative w-1/3 h-50 ">
+            <Image
+              src={"/landing-image.png"}
+              alt={"Santa Tecla"}
+              fill
+              className="object-cover rounded-lg"
+            />
+          </div>
+        </div>
       </section>
     </div>
   );
